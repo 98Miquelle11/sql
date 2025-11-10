e22ec33906561b177ce3e7ea1bd83f4454815e40
Michal Kuropieska <154534560+98Miquelle11@users.noreply.github.com>
2025-11-10 23:04:25 +0100

    char " added.

--- PasjaInformatyki/Part1/query.sql
+++ PasjaInformatyki/Part1/query.sql
@@ -1,6 +1,6 @@
 DROP DATABASE quiz;  -- deletes database "quiz"
 
-CREATE DATABASE quiz  -- creates database "quiz
+CREATE DATABASE quiz  -- creates database "quiz"
 CHARACTER SET utf8mb4  -- full UTF-8 support (including emojis and all Unicode characters)
 COLLATE utf8mb4_polish_ci;  -- Polish-specific collation, case-insensitive (ci), sorts Polish letters properly
 
